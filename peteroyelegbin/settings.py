97686225d95ef7bdc1cde5476be35dde01fc6758
Peter T. Oyelegbin <peteroyelegbin@gmail.com>
2024-09-29 20:28:31 +0100

    Update settings.py

--- peteroyelegbin/settings.py
+++ peteroyelegbin/settings.py
@@ -109,6 +109,7 @@ DATABASES = {
     }
 }
 
+DATABASE_ROUTERS = ['peteroyelegbin.routers.PrimaryReplicaRouter']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
