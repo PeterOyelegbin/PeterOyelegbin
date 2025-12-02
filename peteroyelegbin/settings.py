fe4cac5c7e36116d52d148984de4144b453d70fd
Peter Oyelegbin <peteroyelegbin@gmail.com>
2025-12-02 22:33:29 +0100

    clean up repo

--- peteroyelegbin/settings.py
+++ peteroyelegbin/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com', '.vercel.app', 'peteroyelegbin.com.ng']
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com', '.vercel.app', 'peteroyelegbin.com.ng', 'www.peteroyelegbin.com.ng']
 
 
 # Application definition
